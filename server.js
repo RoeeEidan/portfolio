da92cc8514bfdbb53e227e6d096cd6a88b177489
Roee Eidan <roeeeidan@gmail.com>
2018-11-19 21:59:25 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// Npm Packegaes
+const express = require('express');
+
+const PORT = process.env.PORT || 8080
+
+// Server.
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(function (err, req, res, next) {
+  res.send('/index.html')
+})
+
+// Home page.
+app.get('/', ((req, res) => {
+  res.send('index.html')
+}))
+
+app.get('*', ((req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+}))
+
+app.listen(PORT, () => {
+  console.log(`Server Started on port ${PORT}`);
+});
